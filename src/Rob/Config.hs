2338a82bb8ca09e5d589897f42c6f6833a6d6f35
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-01-20 19:01:58 +0100

    closes #4

--- src/Rob/Config.hs
+++ src/Rob/Config.hs
@@ -9,7 +9,7 @@ import Data.Maybe
 import System.Exit
 import System.FilePath (joinPath)
 import System.Directory (getHomeDirectory, doesFileExist)
-import Data.Yaml (encodeFile, decodeFile)
+import Data.Yaml (encodeFile, decodeFileEither)
 
 -- | Get the config file name
 configFileName :: String
@@ -22,10 +22,14 @@ configFilePath = do
   return $ joinPath [home, configFileName]
 
 -- | Write the config file and return it
-write :: Config -> IO Config
-write config = do
+writeConfig :: Config -> IO Config
+writeConfig config = do
   configFilePath >>= \path -> encodeFile path config
   return config
+
+-- | Read the config file and return it
+readConfig :: FilePath -> IO Config
+readConfig path = either (error . show) id <$> decodeFileEither path
 
 -- | Get the current Config file Data
 -- | If it doesn't exist it will create a new one
@@ -36,13 +40,12 @@ get = do
   if hasConfigPath
     then do
       success $ configFileFound path
-      config <- decodeFile path
-      return $ fromJust config
+      readConfig path
     else do
       warning $ noConfigFileFound configFileName
       flatten info $ configFileCreated path
       -- return an empty Config object and write it in the home directory
-      write $ Config []
+      writeConfig $ Config []
 
 -- | Dispatch the no templates available error
 errorNoTemplatesAvailable :: IO ()
@@ -53,7 +56,7 @@ errorNoTemplatesAvailable = do
 
 -- | Add a new template to the config object and write it
 addTemplate :: Config -> String -> String -> IO Config
-addTemplate (Config templates) name path = write $ Config newTemplates
+addTemplate (Config templates) name path = writeConfig $ Config newTemplates
   where
     newTemplate = Template name path
     newTemplates = if newTemplate `elem` templates then
@@ -63,6 +66,6 @@ addTemplate (Config templates) name path = write $ Config newTemplates
 
 -- | Delete a template from the list of templates
 deleteTemplate :: Config -> String -> IO Config
-deleteTemplate (Config templates) nameToRemove = write $ Config newTemplates
+deleteTemplate (Config templates) nameToRemove = writeConfig $ Config newTemplates
   where
     newTemplates = (\(Template name _) -> name /= nameToRemove) `filter` templates
